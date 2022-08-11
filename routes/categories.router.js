2872604c6584a67845128ec74b3e71cac75d99c9
Mateo Díaz <mateodiaz401@gmail.com>
2022-08-10 22:50:18 -0500

    add category roter

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,55 +1,73 @@
-
 const express = require('express');
 
+const CategoryService = require('./../services/category.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
+
 const router = express.Router();
+const service = new CategoryService();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  })
-});
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.json(
-    {
-      message: 'create',
-      data: body
+router.get('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
     }
-  )
-});
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json(
-    {
-      message: 'update',
-      data: body,
-      id,
+  }
+);
+
+router.post('/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
     }
-  )
-});
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json(
-    {
-      message: 'update',
-      data: body,
-      id,
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
     }
-  )
-});
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json(
-    {
-      message: 'deleted',
-      id,
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({ id });
+    } catch (error) {
+      next(error);
     }
-  )
-})
+  }
+);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const productRouter = require('./products.router')
-const userRouter = require('./user.router')
-const categoriesRouter = require('./categories.router')
+const productRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
+const userRouter = require('./user.router');
+const orderRouter = require('./orders.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1/', router);
   router.use('/products', productRouter);
-  router.use('/users', userRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/users', userRouter);
+  router.use('/orders', orderRouter);
 }
 
 module.exports = routerApi;
